0827b2ed0f69725a3b5f10c10d03019fac20825a
Socrates <socratesmridha@gmail.com>
2018-04-06 22:18:54 -0400

    Few Addons
    New Assests, Fonts, Meterials

---
+++ Split Second (Unity)/Assets/Scripts/FiftyFifty.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FiftyFifty : MonoBehaviour {
+
+
+    public GameObject mainSphere;
+    
+
+    enum Color {Red,Blue,Yellow,Green,Purple,Orange,Grey,Black,White,Pink,Cyan};
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Split Second (Unity)/Assets/Scripts/Sphere_Rotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_Rotation : MonoBehaviour {
+
+    public bool leftOrbit;
+    public bool rightOrbit;
+    public bool main;
+    public Vector3 mainObject;
+    public GameObject mainObject2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (main)
+        {
+            transform.Rotate(Vector3.right, 100 * Time.deltaTime);
+        }
+        else if (leftOrbit)
+        {
+            transform.RotateAround(mainObject, Vector3.right, 250 * Time.deltaTime);
+            transform.RotateAround(mainObject, Vector3.up, 250 * Time.deltaTime);
+        }
+        else if (rightOrbit)
+        {
+            transform.RotateAround(mainObject, Vector3.left, 250 * Time.deltaTime);
+            transform.RotateAround(mainObject, Vector3.down, -250 * Time.deltaTime);
+        }
+       
+    }
+}
